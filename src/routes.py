c07db7613588dd3338609c28d77e31e61d1d3c7c
blancojohn <johnmario.bdc@gmail.com>
2024-06-17 16:15:24 -0400

    Configuración y jwt_required OK

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required
 api = Blueprint('api', __name__)
 
-@api.route('/test')
+@api.route('/test-private')
+@jwt_required()
 def test():
-    return jsonify({"msg": "Testing API Routes"}), 200
+    return jsonify({"msg": "Testing Private API Routes"}), 200
